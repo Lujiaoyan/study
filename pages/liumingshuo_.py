a403235c22db6390c4d6e3fd88f52468db4befdd
Lujiaoyan <153267639+Lujiaoyan@users.noreply.github.com>
2024-02-03 14:13:10 +0800

    Update liumingshuo_.py

--- pages/liumingshuo_.py
+++ pages/liumingshuo_.py
@@ -6,9 +6,18 @@ def page1 ():
     '''兴趣推荐'''
     pass
 
-def page2 ():
-    '''图片处理工具'''
-    pass
+def page_2():
+    '''我的图片处理工具'''
+    st.write(":sunglasses:图片处理小工具:sunglasses:")
+    uploaded_file = st.file_uploader("上传图片", type=['png', 'jpeg', 'jpg'])
+
+    if uploaded_file:
+        filr_name = uploaded_file.name
+        filr_type = uploaded_file.type
+        filr_size = uploaded_file.size
+        img = Image.open(uploaded_file)
+        st.image(img)
+        st.image(img_change(img))
 
 def page3 ():
     '''智慧词典'''
@@ -26,3 +35,17 @@ elif page == '我的智慧词典':
     page3()
 elif page == '我的留言区':
     page4()
+
+
+
+def img_change(img):
+    '''图片处理'''
+    width, height = img.size
+    img_array = img.load()
+    for x in range(width):
+        for y in range(height):
+            r = img_array[x, y][rc]
+            g = img_array[x, y][gc]
+            b = img_array[x, y][bc]
+            img_array[x, y] = [r, g, b]
+    return, img
